73a6048a4604796634549ddcbef859656a8b4239
ippikioukami <memacias2@miners.utep.edu>
2020-11-29 19:23:01 -0700

    removed unecessary folder

--- project/src/p2_IH.c
+++
@@ -1,9 +0,0 @@
-#include <msp430.h>
-#include "switches.h"
-
-void __interrupt_vec(PORT2_VECTOR) Port_2(){  // Use P1 at S2
-  if(P2IFG & SWITCHES){                       
-    P2IFG &= ~SWITCHES;                      // clear interrupts (SW)
-    switch_IH();
-  }
-}
